c9ade3075848529ebe51895a4a6f4c40e8214139
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2016-02-28 11:16:11 -0300

    Alterando versão do Plugin

--- init.rb
+++ init.rb
@@ -1,13 +1,12 @@
 Redmine::Plugin.register :redmine_portfolio_management do
   name 'Redmine Portfolio Management plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Marcus Sacramento'
+  description 'Plugin para permitir a visualização de Portfólio dos projetos da aplicação'
+  version '0.0.2'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   
   menu :top_menu, :portfolio_management, { :controller => 'portfolio_management', :action => 'index' }, :caption => :portfolio_management_menu
-  
   settings :default => {'empty' => true}, :partial => 'settings/redmine_portfolio_management_settings'
   
 end

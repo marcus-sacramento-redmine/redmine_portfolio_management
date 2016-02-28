580cbb86dc76fdcf346ef01e418de241b0790fd6
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2016-02-28 11:43:28 -0300

    Alterando o init para internacionalização
    Ocorre erro ao tentar iniciar os módulos da aplicação

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_portfolio_management do
   name 'Redmine Portfolio Management plugin'
   author 'Marcus Sacramento'
-  description 'Plugin para permitir a visualização de Portfólio dos projetos da aplicação'
+  description 'Plugin para permitir o uso de Portfolio dos projetos'
   version '0.0.2'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'

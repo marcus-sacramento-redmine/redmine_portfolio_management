390d2a7a58e5cdc0c2dc9e7668270cbde690a0b5
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2016-04-12 02:52:26 +0000

    Criação dos Selects Básicos de Porfolios e Projetos

--- lib/redmine_portfolio_management/redmine.rb
+++ lib/redmine_portfolio_management/redmine.rb
@@ -27,12 +27,22 @@ module RedminePortfolioManagement
             custom_field(:portfolio_management_visible_attribute)
         end
         
-        def list_projects_portfolio
-            sql = "SELECT * from #{Project.table_name}"
+        def list_portfolio_values
+            sql = "Select distinct cf.id,cv.Value from #{CustomValue.table_name} cv,#{CustomField.table_name} cf where cf.id = cv.custom_field_id and cf.type = 'ProjectCustomField' and name = '#{portfolio_management_attribute::name}'"
+            portfolios = ActiveRecord::Base.connection.select_all(sql)
+            s= '|'
+            portfolios.each do |portfolio|
+               s<< portfolio['id'] +"-" +portfolio['value'] +"|"
+            end
+            s
+        end
+        
+        def list_projects_portfolio(portfolio_type)
+            sql = "select cv.value,p.id as project_id,p.name,p.identifier,p.parent_id,p.created_on, p.updated_on, p.is_public from #{CustomValue.table_name} cv, #{Project.table_name} p where cv.customized_id = p.id and cv.customized_type = 'Project' and custom_field_id = #{portfolio_type} order by p.created_on"
             projects = ActiveRecord::Base.connection.select_all(sql)
-            s= ' t '
+            s= '|'
             projects.each do |project|
-               s<< project['id'] +"-"
+               s<< project['value'] +"-"+project['name'] +"-"+project['project_id']+"-"+project['created_on']+"-"+project['is_public']+"|"
             end
             s
         end
